6c61b0807afeee184f02024198b63a7d7598ffde
dotspacecore <smdreg@gmail.com>
2022-02-06 10:54:15 -0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Skills from "./components/Skills";
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      {/* <Navbar /> */}
+      <Navbar />
       <About />
       <Projects />
       <Skills />
